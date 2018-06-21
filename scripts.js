85c1349f903e98cd31fc095849e8ca143aab7131
Jens Filipsson <jensfilipsson@gmail.com>
2018-06-21 13:53:39 +0200

    Fix of error message

--- scripts.js
+++ scripts.js
@@ -5,52 +5,63 @@ let postsContainer = document.querySelector('#postsContainer');
 let buttonContainer = document.querySelector('#buttonContainer');
 let loadButton = `<button id="loadButton" class="load-button">Ladda fler filmer</button>`;
 let form = document.querySelector("#searchForm");
+let errorField = document.querySelector("#errorMessage");
 
 function createPosts() {
-    if ((postCounter + 10) >= arrayLength) {
-        addCount = arrayLength;
-        document.querySelector("#loadButton").disabled = "true";
+    if (arrayLength < 1) {
+        let errorMessage = `Hoppsan, vi hittade ingen film som matchade din sökning. Försök gärna igen!`;
+        errorField.insertAdjacentHTML('beforeend', errorMessage);
     } else {
-        addCount = 10;
-    }
-    for (let i = postCounter; i < postCounter + addCount; i += 1) {
-        let postObject = `
+        if ((postCounter + 10) >= arrayLength) {
+            addCount = arrayLength;
+            document.querySelector("#loadButton").disabled = "true";
+        } else {
+            addCount = 10;
+        }
+        for (let i = postCounter; i < postCounter + addCount; i += 1) {
+            let postObject = `
         <div class="movie">
         <img src="${postArray[i].poster}">
         <h2>${postArray[i].title}</h2>
         ${postArray[i].originalTitle ? '<h4>Originaltitel: ' + postArray[i].originalTitle + "</h4>" : ""}
         <p>${postArray[i].summary}</p>
         </div>`;
-        postsContainer.insertAdjacentHTML('beforeend', postObject);
+            postsContainer.insertAdjacentHTML('beforeend', postObject);
+        }
+        postCounter = postCounter + addCount;
     }
-    postCounter = postCounter + addCount;
 }
 
 function fetchPosts(searchTerm) {
-    $.getJSON(`https://javascriptst18.herokuapp.com/trending${searchTerm ? '?title_like=' + searchTerm : ""}`, function (response) {
+    $.getJSON(`https://javascriptst18.herokuapp.com/trending${searchTerm ? '?q=' + searchTerm : ""}`, function (response) {
         postArray = response;
+        buttonContainer.innerHTML = "";
         arrayLength = response.length;
+        if (arrayLength > 0) {
+            buttonContainer.innerHTML = loadButton;
+        }
         createPosts();
     })
 }
 
 document.addEventListener('click', function (e) {
     if (e.target.id === "loadButton") {
+        errorMessage.innerHTML = '';
         createPosts();
     }
 });
 
 searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    errorMessage.innerHTML = '';
     postCounter = 0;
     let inputValue = this.querySelector("#searchTerm").value;
     if (inputValue) {
         postsContainer.innerHTML = '';
         fetchPosts(inputValue);
-        buttonContainer.innerHTML = loadButton;
     } else {
         postsContainer.innerHTML = '';
         fetchPosts();
-        buttonContainer.innerHTML = loadButton;
     }
+    this.querySelector("#searchTerm").value = "";
 });
